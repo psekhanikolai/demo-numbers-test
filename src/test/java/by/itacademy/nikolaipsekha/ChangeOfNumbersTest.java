dd4672e56726e006a341c4e4865f278247b726e3
Nikolai <n91056379@gmail.com>
2025-01-09 09:38:40 +0300

    add all test

--- src/test/java/by/itacademy/nikolaipsekha/ChangeOfNumbersTest.java
+++ src/test/java/by/itacademy/nikolaipsekha/ChangeOfNumbersTest.java
@@ -5,7 +5,25 @@ import org.junit.jupiter.api.Test;
 
 public class ChangeOfNumbersTest {
     @Test
-    public void testChangeOfNumber(){
-        Assertions.assertEquals("a=2, b=3,c= 4 excellent",ChahgeOfNumbers.numbersOfChange(2,3,4) );
+    public void testChangeOfNumber1() {
+        Assertions.assertEquals("a=2, b=3, c=4 excellent", ChahgeOfNumbers.numbersOfChange(2, 3, 4));
+    }
+
+    @Test
+    public void testChangeOfNumber2() {
+        Assertions.assertEquals("a=2, b=3, c=-4 good", ChahgeOfNumbers.numbersOfChange(2, 3, -4));
+
+    }
+
+    @Test
+    public void testChangeOfNumber3() {
+        Assertions.assertEquals("a=2, b=-3, c=-4 fine", ChahgeOfNumbers.numbersOfChange(2, -3, -4));
+
+    }
+
+    @Test
+    public void testChangeOfNumber4() {
+        Assertions.assertEquals("a=-2, b=-3, c=-4 bad", ChahgeOfNumbers.numbersOfChange(-2, -3, -4));
+
     }
 }
